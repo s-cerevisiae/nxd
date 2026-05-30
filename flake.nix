10306545eb2e2bddc499cf9e510af669a55b146e
spore <spore2050@gmail.com>
2026-05-30 18:13:36 +0800

    chore: bump version to 0.3.2

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         };
         packages.default = rustPlatform.buildRustPackage {
           pname = "nxd";
-          version = "0.3.1";
+          version = "0.3.2";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
         };
